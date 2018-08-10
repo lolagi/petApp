f223fb9b4309b8c78bc7c496d79a6cd83408f158
lolagi <carola.gille@web.de>
2018-08-10 20:01:52 +0100

    cleaning up

---
+++ testing/src/App.ts
@@ -0,0 +1,26 @@
+//webserver logic implemented here
+//App will encapsulate the web server
+//mountRoutes mounts the routes served by the server
+
+import * as express from 'express'
+
+class App {
+  public express
+
+  constructor () {
+    this.express = express()
+    this.mountRoutes()
+  }
+
+  private mountRoutes (): void {
+    const router = express.Router()
+    router.get('/', (req, res) => {
+      res.json({
+        message: 'Hello World!'
+      })
+    })
+    this.express.use('/', router)
+  }
+}
+
+export default new App().express
---
+++ testing/src/index.ts
@@ -0,0 +1,13 @@
+//fires up the webserver
+
+import app from './App'
+
+const port = process.env.PORT || 3000
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log(err)
+  }
+
+  return console.log(`server is listening on ${port}`)
+})
